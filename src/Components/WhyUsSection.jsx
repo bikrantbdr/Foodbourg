9cdc5606adf493fe58714489a3faed0cb5e30f83
bikrantbdr <bikrantbdr@gmail.com>
2023-06-06 14:23:35 +0545

    spacing for whyus section updated

--- src/Components/WhyUsSection.jsx
+++ src/Components/WhyUsSection.jsx
@@ -10,13 +10,13 @@ const WhyUsSection = () => {
                     <Box fontSize={"2rem"} fontWeight={"bold"} textAlign={"center"}>Why People Choose FoodBourg?</Box>
                     <Box fontSize={"12px"} color="#718096">Client’s Most Popular Choice</Box>
                 </Flex>
-                <Flex width="90%" justifyContent={"space-evenly"} mt="2rem" wrap={"wrap"}>
+                <Flex width="90%" justifyContent={"space-evenly"} mt="2rem" wrap={"wrap"} gap="12px">
                     {WhyChooseUsData.map((item) => (
-                        <Flex flexDirection={"column"} alignItems={"center"} width={{ base: "50%", md: "45%", lg: "23%" }} key={item.id} pt={`${item.id == 2 | item.id == 4 ? "55px" : "0px"}`} pb={"2rem"} >
+                        <Flex flexDirection={"column"} alignItems={"center"} width={{ base: "60%", md: "27%", lg: "23%" }} key={item.id} pt={{ base: "10px", lg: `${item.id == 2 | item.id == 4 ? "55px" : "0px"}` }} pb={"2rem"} >
                             <Flex fontSize={"1rem"} background={"#C41E24"} color="white" justifyContent={"center"} alignItems={"center"} width="2rem" height="2rem" borderRadius="50%">
                                 {item.id}
                             </Flex>
-                            <Flex width="100%" fontSize={"1rem"} fontWeight={"500"} justifyContent={"left"} mt="0.5rem">{item.title}</Flex>
+                            <Flex width="100%" fontSize={"1rem"} fontWeight={"500"} justifyContent={{ base: "center", lg: "left" }} mt="0.5rem">{item.title}</Flex>
                             <Flex width="100%" fontSize={"12px"} color="#718096" justifyContent={"left"} mt="0.5rem">{item.body}</Flex>
                         </Flex>
                     ))}
